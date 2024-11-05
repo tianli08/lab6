034ba861c85deadf3e70f5dc41c4a763738ba962
Tian Hao Li <tianli081104@gmail.com>
2024-11-04 23:57:23 -0500

    final
    final lab

---
+++ app/src/main/java/com/example/MyDBHandler.java
@@ -0,0 +1,79 @@
+package com.example.myapplication;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "productDB.db";
+    public static final String TABLE_PRODUCTS = "products";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PRODUCTNAME = "productname";
+    public static final String COLUMN_SKU = "SKU";
+
+    public MyDBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " +
+                TABLE_PRODUCTS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY, "
+                + COLUMN_PRODUCTNAME + " TEXT, "
+                + COLUMN_SKU + " INTEGER" + ")";
+        db.execSQL(CREATE_PRODUCTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+
+    public void addProduct(Product product) {
+        try (SQLiteDatabase db = this.getWritableDatabase()) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_PRODUCTNAME, product.getProductName());
+            values.put(COLUMN_SKU, product.getSku());
+            db.insert(TABLE_PRODUCTS, null, values);
+        }
+    }
+
+    public Product findProduct(String productname) {
+        Product product = null;
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " = ?";
+
+        try (SQLiteDatabase db = this.getReadableDatabase();
+             Cursor cursor = db.rawQuery(query, new String[]{productname})) {
+
+            if (cursor.moveToFirst()) {
+                product = new Product();
+                product.setID(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID)));
+                product.setProductName(cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_PRODUCTNAME)));
+                product.setSku(cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_SKU)));
+            }
+        }
+        return product;
+    }
+
+    public boolean deleteProduct(String productname) {
+        boolean result = false;
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + " = ?";
+
+        try (SQLiteDatabase db = this.getWritableDatabase();
+             Cursor cursor = db.rawQuery(query, new String[]{productname})) {
+
+            if (cursor.moveToFirst()) {
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID));
+                db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
+                result = true;
+            }
+        }
+        return result;
+    }
+}
